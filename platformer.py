d57be55cc808ac68875fdda6e30aa078fee9b980
Thaksha <thaksha.mangaleswaran@mail.utoronto.ca>
2023-05-30 13:47:44 -0400

    added grass and dirt

--- platformer.py
+++ platformer.py
@@ -8,9 +8,14 @@ screen_width = 1000
 screen_height = 1000
 
 # creates a game window
-screen = pygame.display.set_mode((screen_width, screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height),
+                                 pygame.RESIZABLE)
+
 # names game window
 pygame.display.set_caption('Platformer Game')
+
+# define game variables
+tile_size = 50
 
 # load images
 
@@ -18,15 +23,93 @@ sun_img = pygame.image.load('img/sun.png')
 bg_img = pygame.image.load('img/sky.png')
 
 
+def draw_grid():
+    for line in range(0, 20):
+        # (255, 255, 255) = colour white
+        # (0, line * tile_size) = x, y coord
 
+        pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size),
+                         (screen_width, line * tile_size))
+        pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0),
+                         (line * tile_size, screen_height))
 
+class World():
+    def __init__(self, data):
+
+        self.tile_list = []
+        dirt_img = pygame.image.load('img/dirt.png')
+        grass_img = pygame.image.load('img/grass.png')
+
+        # go through each tile one by one
+        row_count = 0
+
+        for row in data:
+            col_count = 0
+            for tile in row:
+
+                # tile is dirt
+                if tile == 1:
+                    # scale it to fit tile
+                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+                    # create rectangle from image
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+
+                # tile is grass
+                if tile == 2:
+                    # scale it to fit tile
+                    img = pygame.transform.scale(grass_img,
+                                                 (tile_size, tile_size))
+                    # create rectangle from image
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+
+world_data = [
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1],
+[1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1],
+[1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1],
+[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1],
+[1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+world = World(world_data)
 # keeps window displaying until run is false
 run = True
 while run:
 
     # put images onto display
-    screen.blit(bg_img, (0,0))
+    screen.blit(bg_img, (0, 0))
     screen.blit(sun_img, (100, 100))
+
+    world.draw()
+    draw_grid()
 
     # checks whether user clicks x to quit
     for event in pygame.event.get():
@@ -36,4 +119,3 @@ while run:
     # update display window with instruction
     pygame.display.update()
 pygame.quit()
-
