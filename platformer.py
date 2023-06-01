0b528e34e49d8faa054a03014474e7fdfe0245f3
Thaksha <thaksha.mangaleswaran@mail.utoronto.ca>
2023-06-01 18:13:16 -0400

    added animation for left and right movement

--- platformer.py
+++ platformer.py
@@ -4,8 +4,12 @@ from pygame.locals import *
 # initialize pygame
 pygame.init()
 
+clock = pygame.time.Clock()
+fps = 60
+
+
 screen_width = 1000
-screen_height = 1000
+screen_height = 1000 # make 500 to see the guy
 
 # creates a game window
 screen = pygame.display.set_mode((screen_width, screen_height),
@@ -32,6 +36,101 @@ def draw_grid():
                          (screen_width, line * tile_size))
         pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0),
                          (line * tile_size, screen_height))
+class Player():
+
+    def __init__(self, x, y):
+        self.images_right = []
+        self.images_left = []
+        self.index = 0
+        self.counter = 0
+
+        for num in range(1, 5):
+            img_right = pygame.image.load(f'img/guy{num}.png')
+            img_right = pygame.transform.scale(img_right,(40, 80))
+            img_left = pygame.transform.flip(img_right, True, False)
+            self.images_left.append(img_left)
+            self.images_right.append(img_right)
+
+        self.image = self.images_right[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.vel_y = 0
+        self.jumped = False
+        self.direction = 0
+
+    def update(self):
+
+        dx = 0
+        dy = 0
+
+        walk_cooldown = 5
+
+        #get keypresses
+        key = pygame.key.get_pressed()
+
+        if key[pygame.K_SPACE] and self.jumped == False:
+            self.vel_y = - 15
+            self.jumped = True
+
+        if key[pygame.K_SPACE] == False:
+            self.jumped = False
+
+        if key[pygame.K_LEFT]:
+            dx -= 5
+            self.counter += 1
+            self.direction = -1
+
+        if key[pygame.K_RIGHT]:
+            dx += 5
+            self.counter += 1
+            self.direction = 1
+
+        if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
+            self.counter = 0
+            self.index = 0
+
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
+
+        # handle animation
+        if self.counter > walk_cooldown:
+            self.counter = 0
+            self.index += 1
+            if self.index == len(self.images_right):
+                self.index = 0
+
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
+
+
+
+
+        # add gravity
+        self.vel_y += 1
+
+
+        dy += self.vel_y
+
+        # check for collison
+
+        # update player coordinates
+        self.rect.x += dx
+        self.rect.y += dy
+
+        # prevnts player from going off the screen
+        if self.rect.bottom > screen_height:
+            self.rect.bottom = screen_height
+            dy = 0
+
+        # draw player onto screen
+        screen.blit(self.image, self.rect)
 
 class World():
     def __init__(self, data):
@@ -99,17 +198,22 @@ world_data = [
 [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
+player = Player(100, screen_height - 130)
 world = World(world_data)
 # keeps window displaying until run is false
 run = True
 while run:
+
+    # fixes frame rate
+    clock.tick(fps)
 
     # put images onto display
     screen.blit(bg_img, (0, 0))
     screen.blit(sun_img, (100, 100))
 
     world.draw()
-    draw_grid()
+    player.update()
+    #draw_grid()
 
     # checks whether user clicks x to quit
     for event in pygame.event.get():
